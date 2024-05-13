21d8f557ee26bb3804bc831c9e293004f68643ef
orkit <ryan@dias.se>
2024-05-13 12:24:20 +0200

    Fixed issue #170

--- resources/views/dsv/homeblade.blade.php
+++ resources/views/dsv/homeblade.blade.php
@@ -2,8 +2,10 @@
 @include('dsvheader')
 @include('navbar.navbar')
 @include('layouts.breadcrumbs')
+
 <!-- Sidenav with content -->
-<div class="flex h-screen overflow-hidden bg-white dark:bg-gray-800 dark:text-white">
+<div class="flex overflow-hidden bg-white dark:bg-gray-800 dark:text-white">
     @include('partials.page')
 </div>
+
 @include('layouts.darktoggler')
