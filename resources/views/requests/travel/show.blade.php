9e987e2dccbaff01aec84dc0f09dd19128be417a
orkit <ryan@dias.se>
2024-06-10 11:10:22 +0200

    Changed email sender names

--- resources/views/requests/travel/show.blade.php
+++ resources/views/requests/travel/show.blade.php
@@ -129,7 +129,7 @@
                 <label for="expenses" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __("Expenses") }}</label>
                 <br>
                 <div class="w-full">
-                    <label for="flight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __("Flight") }} </label>
+                    <label for="flight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __("Travel (Plane, train, etc)") }} </label>
                     <div class="font-mono bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block
                     w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         {{$tr->flight ?? 0}} SEK
