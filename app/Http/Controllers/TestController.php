56a76065763e8fc06c7064274005d0b3cdccbb07
orkit <ryan@dias.se>
2024-11-13 16:24:56 +0100

    Fix navbar titles from wrapping

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,33 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Dashboard;
-use App\Models\ProjectProposal;
-use App\Models\SettingsFo;
-use App\Models\User;
-use Carbon\Carbon;
-use GuzzleHttp\Client;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-
 class TestController extends Controller
 {
 
    public function test()
    {
-       $user = Auth::user();
-
-       $proposals = ProjectProposal::with('dashboard')
-           ->where('id', '9d768256-9084-4bf6-8fed-363d064b4387')
-           ->get();
-        foreach ($proposals as $proposal) {
-            dd($proposal->allowEdit());
-        }
-
-
-       /*return (new \Statamic\View\View)
+       return (new \Statamic\View\View)
            ->template('home.partials.search.staff')
-           ->layout('mylayout');*/
+           ->layout('mylayout');
    }
 }
