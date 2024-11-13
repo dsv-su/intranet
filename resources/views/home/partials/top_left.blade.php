a49b72a8404e0b80fa4f944e04bd3b3e60cc785f
orkit <ryan@dias.se>
2024-11-13 20:58:30 +0100

    Activate prototype test for project proposal

--- resources/views/home/partials/top_left.blade.php
+++ resources/views/home/partials/top_left.blade.php
@@ -1,4 +1,4 @@
-<div class="md:order-1 relative pl-6 flex flex-col justify-center items-start {{--}}md:min-h-[230px]{{--}} {{--}}md:h-fit{{--}} text-center rounded-xl
+<div class="md:order-1 relative pl-6 flex flex-col justify-center items-start text-center rounded-xl
                 border border-susecondary dark:border-gray-800">
     <div class="mt-0 text-left">
         <h3 class="text-lg md:text-xl font-semibold text-gray-800 dark:text-gray-200">
@@ -21,7 +21,7 @@
             </svg>
         </a>
         <br>
-        <a href="https://serviceportalen.su.se" aria-label="Serviceportal email" class="mt-4 inline-flex items-center gap-x-1.5 text-blue-800 font-medium dark:text-gray-200">
+        <a href="https://serviceportalen.su.se" aria-label="Serviceportal email" class="mt-2 inline-flex items-center gap-x-1.5 text-blue-800 font-medium dark:text-gray-200">
             Serviceportalen
             <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
                 <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
