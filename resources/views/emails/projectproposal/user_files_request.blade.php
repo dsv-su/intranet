9418a10fdf4ed1ef05249209cfcd04f80651e745
orkit <ryan@dias.se>
2025-02-12 20:07:02 +0100

    Notification for fileupload

--- resources/views/emails/projectproposal/user_files_request.blade.php
+++ resources/views/emails/projectproposal/user_files_request.blade.php
@@ -4,14 +4,18 @@
 <br><br>
 ---
 <br>
-To financial officer, {{$fo->name}}
+To User(Project proposer)
 <br>
 ---
 <br>
-New Project Proposal Submitted for Review <br><br>
-Dear Finacial Officer,
+Your Project Proposal Submitted for Review has been preapproved<br><br>
+Dear {{$user->name}},
 <br><br>
-A new <strong>{{Illuminate\Support\Str::upper($dashboard->type)}}</strong> has been submitted and is now available for your review.
+Your project proposal, submitted on {{Carbon\Carbon::createFromTimestamp($dashboard->created)->format('Y-m-d')}},
+has been reviewed by the Unit Head(s) and the Vice Head and has been reapproved for further processing.
+<br><br>
+Please upload the <strong>DSV budget file</strong>, a <strong>short description of the proposal</strong>, and any <strong>additional relevant files</strong> for review by the <strong>Financial Officer</strong>.
+<br><br>
 Here's a quick overview of the request:
 <br><br>
 <b>Proposal:</b> {{$dashboard->name}}
@@ -20,17 +24,17 @@ Here's a quick overview of the request:
 <br><br>
 <b>Unit Head(s):</b>
 @foreach($dashboard->unit_heads as $head)
-    {{ \App\Models\User::find($head)->name }}
-    ,
+{{ \App\Models\User::find($head)->name }}
+,
 @endforeach
 <br><br>
 <b>Created:</b> {{Carbon\Carbon::createFromTimestamp($dashboard->created)->format('Y-m-d')}}
 <br><br>
-<b>ProposalID:</b> {{$dashboard->request_id}}
-<br><br>
 <b>Approved by vice head:</b> {{Carbon\Carbon::parse($dashboard->updated_at)->format('Y-m-d')}}
 <br><br>
-You can review the details and take necessary action by accessing the proposal through the following link:
+<b>ProposalID:</b> {{$dashboard->request_id}}
+<br><br>
+You can review the details and upload the requested files by accessing the proposal through the following link:
 <br><br>
 <a href="{{ url('') }}/pp/review/{{$dashboard->request_id}}">Direct link to {{$dashboard->name}}</a>
 <br><br>
