321a081c1a3e8825ab103073e35795bc9e7ba321
orkit <ryan@dias.se>
2025-05-20 14:48:16 +0200

    Unlocked stats view

--- resources/views/pp/partials/header.blade.php
+++ resources/views/pp/partials/header.blade.php
@@ -33,22 +33,21 @@
                     </svg>
                     <span class="sr-only">Intranet</span>
                 </a>
-                @can('vice_head')
-                    <a type="button"
-                       data-tooltip-target="stats-tooltip"
-                       href="{{route('pp-stats')}}"
-                       class="size-[18px] sm:size-[38px] relative inline-flex justify-center items-center gap-x-2 text-sm font-semibold
-                        rounded-full border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none
-                        focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white
-                        dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
-                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                             stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M22 12h-4l-3 9L9 3l-3 9H2"/>
-                        </svg>
-                    </a>
-                @endcan
-                @can('vice_head')
+                <!-- Stats -->
+                <a type="button"
+                   data-tooltip-target="stats-tooltip"
+                   href="{{route('pp-stats')}}"
+                   class="size-[18px] sm:size-[38px] relative inline-flex justify-center items-center gap-x-2 text-sm font-semibold
+                    rounded-full border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-none
+                    focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white
+                    dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
+                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                         stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"/>
+                    </svg>
+                </a>
+                <!-- Settings -->
                 <a type="button"
                     data-tooltip-target="vice-settings-tooltip"
                    href="{{route('vice_settings')}}">
@@ -56,7 +55,6 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                     </svg>
-
                 </a>
                 <!-- Admin -->
                 <a type="button"
@@ -67,19 +65,21 @@
                     </svg>
 
                 </a>
-                @endcan
+
                 <!-- New proposal -->
-                <a type="button"
+                <a role="button"
                    data-tooltip-target="add-proposal-tooltip"
                    href="{{route('new-project')}}"
-                   class="size-[18px] sm:size-[24px] relative inline-flex justify-center items-center gap-x-2 text-sm font-semibold
-                        rounded-full border border-transparent text-blue-500 hover:bg-blue-100 focus:outline-none
-                        focus:bg-blue-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white
-                        dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
-                    <svg class="shrink-0 size-6 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                   class="py-1.5 px-2.5 inline-flex items-center gap-x-1.5 text-xs font-medium rounded-md border
+                      border-blue-600 bg-white text-gray-800 shadow hover:bg-gray-50 focus:outline-none focus:bg-gray-50
+                      disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700
+                      dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
+                    New PP
+                    <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M5 12h14"></path>
+                        <path d="m12 5 7 7-7 7"></path>
                     </svg>
-                    <span class="sr-only">Activity</span>
                 </a>
                 <!-- end new creation flow -->
             </div>
