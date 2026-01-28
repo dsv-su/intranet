9ee5f2f2cce20d43e16b0b00e929d05d174080e8
orkit <ryan@dias.se>
2026-01-28 14:34:44 +0100

    Switched to minimal paginator

--- resources/views/livewire/pp/my-project-proposal-search.blade.php
+++ resources/views/livewire/pp/my-project-proposal-search.blade.php
@@ -11,13 +11,17 @@
             </div>
 
             <!-- Pagination -->
-            @if($proposals->hasPages())
+            {{--}}@if($proposals->hasPages())
             <div class="mb-4 bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
                 <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                     {{$proposals->links()}}
                 </div>
             </div>
+            @endif{{--}}
+            @if ($proposals->hasPages())
+                {{ $proposals->links() }}
             @endif
+
         </div>
     </div>
 </div>
